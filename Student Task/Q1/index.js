933253c2d0b35ab89962f11d459545bfdfbe8470
23Atul <atulraman23@gmail.com>
2025-01-22 03:12:47 +0530

    Q1

--- Student Task/Q1/index.js
+++ Student Task/Q1/index.js
@@ -1,3 +1,4 @@
-let h1 = React.createElement("h1",{}, `"Atul", 24, "Ranchi"`)
+
+let h1 = React.createElement("h1",{className:"heading", id:"head"}, [`"Atul", 24, "Ranchi"`])
 
 ReactDOM.createRoot(document.querySelector("#root")).render(h1)
