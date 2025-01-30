fa11f7703afd6037b57153813a3144e6811c9fc2
23Atul <atulraman23@gmail.com>
2025-01-30 11:14:17 +0530

    using manual re render

--- Day3/Live Class/useState.js
+++ Day3/Live Class/useState.js
@@ -1,22 +1,56 @@
 // counter 
 
-let count=0
-const Counter = ()=>{
-    const incrementFunc =()=>{
-        count++;
-        console.log(count);
-    }
-    return(
-        <>
-            <h1>Counter : {count}</h1>
-            <button onClick={incrementFunc}>Increment</button>
-        </>
-    )
-};
+// let count=0
+// const Counter = ()=>{
+//     const incrementFunc =()=>{
+//         count++;
+//         console.log(count);
+//     }
+//     return(
+//         <>
+//             <h1>Counter : {count}</h1>
+//             <button onClick={incrementFunc}>Increment</button>
+//         </>
+//     )
+// };
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<Counter/>)
+// ReactDOM.createRoot(document.querySelector("#root")).render(<Counter/>)
 
 // the function will get called each time we press the button but the output will not show in webpage as we are not rentering the Counter component again and again..we are just calling it.
 // perfect for JS but for React it needs to re render everytime it gets updated.
+
+
+
+
+
+
+// lets render the function everytime its get called.
+
+
+// let count = 0
+// const Counter = () => {
+//     const incrementFunc = () => {
+//         count++;
+//         console.log(count);
+//         ReactDOM.createRoot(document.querySelector("#root")).render(<Counter />)
+//     }
+//     return (
+//         <>
+//             <h1>Counter : {count}</h1>
+//             <button onClick={incrementFunc}>Increment</button>
+//         </>
+//     )
+// };
+
+// ReactDOM.createRoot(document.querySelector("#root")).render(<Counter />)
+
+// but this is not the right way; we should not manually re render each and every function like this.
+
+
+
+
+
+
+// to overcome this we use useState Hook.
 
 
