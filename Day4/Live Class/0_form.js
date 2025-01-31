d2aea1ffe51f8a2e1345a46c5ae89bbb447cd421
23Atul <atulraman23@gmail.com>
2025-01-31 23:10:24 +0530

    form

--- Day4/Live Class/0_form.js
+++ Day4/Live Class/0_form.js
@@ -5,15 +5,15 @@
 
 const App1 = () =>{
 
-    const [user1,setUser]=React.useState("")
-    const [email1,setEmail]=React.useState("")
+    const [user1,setUser1]=React.useState("")
+    const [email1,setEmail1]=React.useState("")
 
 
-    const handleUserName=(e)=>{
+    const handleUserName1=(e)=>{
         setUser(e.target.value)
     }
 
-    const handleEmail=(e)=>{
+    const handleEmail1=(e)=>{
         setEmail(e.target.value)
     }
 
@@ -22,8 +22,8 @@ const App1 = () =>{
     return(
         <>
             <form>
-                <input onChange={handleUserName} type="text" placeholder="enter username"></input>
-                <input onChange={handleEmail} type="email" placeholder="enter email"></input>
+                <input onChange={handleUserName1} type="text" placeholder="enter username"></input>
+                <input onChange={handleEmail1} type="email" placeholder="enter email"></input>
                 <input type="submit" ></input>
 
 
@@ -37,3 +37,60 @@ const App1 = () =>{
 
 
 
+
+
+
+
+const App2 = () => {
+// we are capturing usename,email phone so best data type to store them is object, so we take empty obj as initial value with keys.
+    const [ formData, setformData] = React.useState({username:"", email:"",phone:""})
+
+
+// handling the all input values using single function instead of creating different function for each input.
+    const handleChange=(e)=>{
+
+        // console.log(e.target.name, e.target.value);
+        //name: username, value: Atul
+        //name: email, value:atul@23
+        //name: phone, value:1234
+
+        const {name , value} = e.target;
+        setformData({...formData,[name]:value})
+
+        // spreading the initial object and filling the value to each key one by one
+        //step1:  setformData({username:"",email:"",phone:"" username:"Atul"})
+        //step2:  setformData({username:"Atul, email:"", phone:"", email:"atul@23"})
+        //step3:  setformData({username:"Atul, email:"atul@23", phone:"", phone:"123456"})
+
+        
+    }
+
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(formData);
+
+        // here we are just printing the data when user submits it...next we will see how we can post the same data to database when use submits it.
+    }
+
+    console.log(formData);
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+{/* add name in each input and keep name same as what you have kept in state variable initial value */}
+                <input onChange={ handleChange } type="text" placeholder="enter username" name="username"></input> 
+                <input onChange={ handleChange} type="email" placeholder="enter email" name="email"></input>
+                <input onChange={ handleChange } type="number" placeholder="enter phone" name="phone"></input>
+{/* if any new input tag is added just we have to update the key in useState  */}
+                <input type="submit"></input>
+            </form>
+                
+
+        </>
+        
+
+    )
+
+}
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<App2/>)
